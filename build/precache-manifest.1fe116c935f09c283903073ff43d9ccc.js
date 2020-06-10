54ab947b0756f94227b50a0df87a642a98f8f2a3
ullataponen <ulla.taponen@gmail.com>
2020-06-10 17:00:02 +0200

    add http test files for heroku, FE build

---
+++ build/precache-manifest.1fe116c935f09c283903073ff43d9ccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad23f35cede138c46ff8ce0e7593a76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01a97b7c82d4322f7d25",
+    "url": "/static/css/main.ae55aa60.chunk.css"
+  },
+  {
+    "revision": "651b71704cc885073c84",
+    "url": "/static/js/2.3850bdf0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3850bdf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01a97b7c82d4322f7d25",
+    "url": "/static/js/main.f12f9594.chunk.js"
+  },
+  {
+    "revision": "3305686cfe022fad0350",
+    "url": "/static/js/runtime-main.4ce057ac.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 morgan.token("body", function (req, res) {
 	return JSON.stringify(req.body);
